82556c9e1828ea0169090579b406e3a95ee39bae
mirzoyanani <animirzoyan.ait@gmail.com>
2023-09-25 18:27:49 +0400

    set-up server

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+dotenv.config();
+
+interface CorsOptions {
+  origin: string;
+  credentials: boolean;
+}
+type Port = number | string;
+const PORT: Port = process.env.PORT || 9090;
+
 const app = express();
 
-app.use(cors);
-console.log(5);
+const corsOptions: CorsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/images", express.static(path.join(path.resolve(), "images")));
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
